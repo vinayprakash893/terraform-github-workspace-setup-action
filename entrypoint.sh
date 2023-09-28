217439d75ea8d5d486f083982f675f9bb386b513
Vinay <vinayprakash.allsober@gmail.com>
2023-09-28 22:53:36 +0530

    sasa

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,10 +22,11 @@ wid=$(curl -s --header "Authorization: Bearer $TF_TOKEN" --header "Content-Type:
 # while [ $i -lt $x ]
 # do
 #   curl -s --header "Authorization: Bearer $TF_TOKEN" --header "Content-Type: application/vnd.api+json" --request DELETE "https://$TF_HOST/api/v2/workspaces/$wid/vars/$(cat vars.json | jq -r ".data[$i].id")" > logs.txt
-#   i=`expr $i + 1`
+#   i=`expr $i + 1`  
 # done
 
 #Clean only replacing variables
+curl -s --header "Authorization: Bearer $TF_TOKEN" --header "Content-Type: application/vnd.api+json" "https://$TF_HOST/api/v2/workspaces/$wid/vars" > fullvars.json
 for k in $(jq '.vars | keys | .[]' variables.json); do
     value=$(jq -r ".vars[$k]" variables.json);
 
@@ -36,11 +37,12 @@ for k in $(jq '.vars | keys | .[]' variables.json); do
 
     printf "\nDelete variable %s" "$key"
     printf "\nVinay"
-    curl -s --header "Authorization: Bearer $TF_TOKEN" --header "Content-Type: application/vnd.api+json" "https://$TF_HOST/api/v2/workspaces/$wid/vars" > vars.json
-    id=$(cat vars.json | jq -r --arg key "$key" '.data[] | select(.attributes.key == $key) | .id')
-    printf "\nDelete variable %s" "$id"
+    
+    id=$(cat fullvars.json | jq -r --arg key "$key" '.data[] | select(.attributes.key == $key) | .id')
+    printf "\nId of the variable %s" "$id"
     printf "\n"
     curl -s --header "Authorization: Bearer $TF_TOKEN" --header "Content-Type: application/vnd.api+json" --request DELETE "https://$TF_HOST/api/v2/workspaces/$wid/vars/$id"
+    printf "\nDeleted variable %s" "$key"
 done
 
 #Create variables
